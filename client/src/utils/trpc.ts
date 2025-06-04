cafb396b445830ac759d48f09a26010021be687f
Ben Blaker <ben_blaker@hotmail.com>
2025-06-04 01:27:05 +0100

    Add QueryCaching

--- client/src/utils/trpc.ts
+++ client/src/utils/trpc.ts
@@ -3,7 +3,14 @@ import { createTRPCClient, httpBatchLink } from "@trpc/client";
 import { createTRPCOptionsProxy } from "@trpc/tanstack-react-query";
 import type { AppRouter } from "../../../shared/index.ts";
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // 5 minutes
+      gcTime: 1000 * 60 * 10, // 10 minutes
+    },
+  },
+});
 
 const trpcClient = createTRPCClient<AppRouter>({
   links: [httpBatchLink({ url: "http://localhost:3001/trpc" })],
