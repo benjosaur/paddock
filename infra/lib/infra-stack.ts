3f797e3fea1a7bad3935448a29622be2b1504765
Ben Blaker <ben_blaker@hotmail.com>
2025-06-10 16:20:07 +0100

    Add DNS + SSL

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -3,10 +3,14 @@ import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import * as certificatemanager from "aws-cdk-lib/aws-certificatemanager";
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const domainName = "paddock.health";
+    const subdomainName = "www.paddock.health";
 
     const s3CorsRule: s3.CorsRule = {
       allowedMethods: [s3.HttpMethods.GET, s3.HttpMethods.HEAD],
@@ -26,6 +30,14 @@ export class InfraStack extends cdk.Stack {
     const oai = new cloudfront.OriginAccessIdentity(this, "OAI");
     s3Bucket.grantRead(oai);
 
+    // SSL Certificate - Created in us-east-1 using cross-region reference
+    const certificate = certificatemanager.Certificate.fromCertificateArn(
+      this,
+      "Certificate",
+      // You'll need to replace this with your actual certificate ARN after creating it
+      `arn:aws:acm:us-east-1:${this.account}:certificate/e4f64a88-53f6-4a87-ad97-441c1b238646`
+    );
+
     const distribution = new cloudfront.CloudFrontWebDistribution(
       this,
       "Distribution",
@@ -44,6 +56,15 @@ export class InfraStack extends cdk.Stack {
             ],
           },
         ],
+        // Add custom domain configuration
+        viewerCertificate: cloudfront.ViewerCertificate.fromAcmCertificate(
+          certificate,
+          {
+            aliases: [domainName, subdomainName],
+            securityPolicy: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
+            sslMethod: cloudfront.SSLMethod.SNI,
+          }
+        ),
         errorConfigurations: [
           {
             errorCode: 404,
@@ -56,6 +77,8 @@ export class InfraStack extends cdk.Stack {
             responsePagePath: "/index.html",
           },
         ],
+        // Redirect HTTP to HTTPS
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
       }
     );
 
@@ -69,6 +92,11 @@ export class InfraStack extends cdk.Stack {
     new cdk.CfnOutput(this, "CloudFrontURL", {
       value: distribution.distributionDomainName,
       description: "CloudFront Distribution URL",
+    });
+
+    new cdk.CfnOutput(this, "DomainURL", {
+      value: `https://${domainName}`,
+      description: "Custom Domain URL",
     });
   }
 }
